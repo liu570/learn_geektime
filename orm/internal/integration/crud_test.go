76489fb2a504d11751ee13da504ad2a6e6f32d74
liu <ktcliuwenjie@gmail.com>
2023-05-23 19:37:39 +0800

    增加了一个update的集成测试

--- orm/internal/integration/crud_test.go
+++ orm/internal/integration/crud_test.go
@@ -16,7 +16,7 @@ import (
 	"testing"
 )
 
-type InsertTestSuite struct {
+type TestSuite struct {
 	suite.Suite
 	db *orm.DB
 
@@ -25,7 +25,7 @@ type InsertTestSuite struct {
 }
 
 // 类似于init函数
-func (i *InsertTestSuite) SetupSuite() {
+func (i *TestSuite) SetupSuite() {
 	db, err := orm.Open(i.driver, i.dsn)
 	if err != nil {
 		i.T().Fatal(err)
@@ -33,7 +33,7 @@ func (i *InsertTestSuite) SetupSuite() {
 	i.db = db
 	i.db.Wait()
 }
-func (i *InsertTestSuite) TestInsert() {
+func (i *TestSuite) TestInsert() {
 	t := i.T()
 	db := i.db
 	testCases := []struct {
@@ -56,6 +56,24 @@ func (i *InsertTestSuite) TestInsert() {
 			wantData: &TestModel{
 				Id:       13,
 				LastName: &sql.NullString{String: "liu", Valid: true},
+			},
+		},
+		{
+			name: "insert single",
+			i: orm.NewInserter[TestModel](db).Values(
+				&TestModel{
+					Id:        14,
+					FirstName: "WenJie",
+					Age:       22,
+					LastName:  &sql.NullString{String: "Liu", Valid: true},
+				},
+			),
+			affected: 1,
+			wantData: &TestModel{
+				Id:        14,
+				FirstName: "WenJie",
+				Age:       22,
+				LastName:  &sql.NullString{String: "Liu", Valid: true},
 			},
 		},
 	}
@@ -82,16 +100,55 @@ func (i *InsertTestSuite) TestInsert() {
 	}
 }
 
+func (u *TestSuite) TestUpdate() {
+	t := u.T()
+	db := u.db
+	testCases := []struct {
+		name string
+		u    *orm.Updater[TestModel]
+		// 受影响行数
+		affected int64
+		wantErr  error
+		wantData *TestModel
+	}{
+		{
+			name: "update single",
+			u: orm.NewUpdater[TestModel](db).Update(&TestModel{
+				LastName: &sql.NullString{String: "update", Valid: true},
+			}).Set(orm.C("LastName")).Where(orm.C("Id").EQ(13)),
+			affected: 1,
+			wantData: &TestModel{
+				Id:       13,
+				LastName: &sql.NullString{String: "update", Valid: true},
+			},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			res := tc.u.Exec(context.Background())
+			affected, err := res.RowsAffected()
+			assert.Equal(t, tc.wantErr, err)
+			if err != nil {
+				return
+			}
+			assert.Equal(t, tc.affected, affected)
+			data, err := orm.NewSelector[TestModel](db).Where(orm.C("Id").EQ(13)).Get(context.Background())
+			require.NoError(t, err)
+			assert.Equal(t, tc.wantData, data)
+		})
+	}
+}
+
 func TestMySQL(t *testing.T) {
 
-	suite.Run(t, &InsertTestSuite{
+	suite.Run(t, &TestSuite{
 		driver: "mysql",
 		dsn:    "root:root@tcp(127.0.0.1:13306)/integration_test",
 	})
 }
 
 func TestSQLite(t *testing.T) {
-	suite.Run(t, &InsertTestSuite{
+	suite.Run(t, &TestSuite{
 		driver: "sqlite3",
 		dsn:    "file:test.db?cache=shared&mode=memory",
 	})
