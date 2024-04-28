0ca0d3dfe4e0205928aba6e38cd4c5c2d27f83b9
liu <ktcliuwenjie@gmail.com>
2024-04-28 11:36:47 +0800

    grpc:加权轮询实现 注册中心传递权重信息ing

--- micro/loadbalance/round_robin/weight_balancer.go
+++ micro/loadbalance/round_robin/weight_balancer.go
@@ -3,40 +3,81 @@ package round_robin
 import (
 	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/balancer/base"
+	"math"
+	"strconv"
+	"sync"
 )
 
 type WeightBalancer struct {
-	connections []weightSubConn
-	length      int32
-	index       int32
+	connections []*weightSubConn
+	mutex       sync.Mutex
 }
 
-func (w WeightBalancer) Pick(info balancer.PickInfo) (balancer.PickResult, error) {
-	//TODO implement me
-	panic("implement me")
+func (w *WeightBalancer) Pick(info balancer.PickInfo) (balancer.PickResult, error) {
+	if w.connections == nil || len(w.connections) == 0 {
+		return balancer.PickResult{}, balancer.ErrNoSubConnAvailable
+	}
+	var resConn *weightSubConn
+	var totalWeight uint32
+	for _, conn := range w.connections {
+		conn.mutex.Lock()
+		totalWeight += conn.efficientWeight
+		conn.currentWeight += conn.efficientWeight
+		if resConn == nil || resConn.currentWeight < conn.currentWeight {
+			resConn = conn
+		}
+		conn.mutex.Unlock()
+	}
+	resConn.mutex.Lock()
+	resConn.currentWeight -= totalWeight
+	resConn.mutex.Unlock()
+	return balancer.PickResult{
+		SubConn: resConn.conn,
+		Done: func(info balancer.DoneInfo) {
+			resConn.mutex.Lock()
+			if info.Err != nil && resConn.efficientWeight == 0 {
+				return
+			}
+			if info.Err == nil && resConn.efficientWeight == math.MaxUint32 {
+				return
+			}
+			if info.Err != nil {
+				resConn.efficientWeight--
+			} else {
+				resConn.efficientWeight++
+			}
+			resConn.mutex.Unlock()
+		},
+	}, nil
 }
 
 type WeightBalanceBuilder struct{}
 
 func (w *WeightBalanceBuilder) Build(info base.PickerBuildInfo) balancer.Picker {
-	connections := make([]weightSubConn, 0, len(info.ReadySCs))
-	for c := range info.ReadySCs {
-		// TODO:加权轮询
-		wConn := weightSubConn{
-			conn: c,
+	connections := make([]*weightSubConn, 0, len(info.ReadySCs))
+	for sub, subInfo := range info.ReadySCs {
+		weightStr := subInfo.Address.Attributes.Value("weight").(string)
+		weight, err := strconv.ParseInt(weightStr, 10, 64)
+		if err != nil {
+			panic(err)
 		}
-		connections = append(connections, wConn)
+		wConn := weightSubConn{
+			conn:            sub,
+			weight:          uint32(weight),
+			efficientWeight: uint32(weight),
+			currentWeight:   uint32(weight),
+		}
+		connections = append(connections, &wConn)
 	}
 	return &WeightBalancer{
 		connections: connections,
-		index:       -1,
-		length:      int32(len(connections)),
 	}
 }
 
 type weightSubConn struct {
 	conn            balancer.SubConn
-	weight          int
-	currentWeight   int
-	efficientWeight int
+	mutex           sync.Mutex
+	weight          uint32
+	currentWeight   uint32
+	efficientWeight uint32
 }
--- micro/registry/types.go
+++ micro/registry/types.go
@@ -16,6 +16,11 @@ type ServiceInstance struct {
 	Name string
 	// 这里表示是你的定位信息
 	Address string
+
+	// 其它信息
+	// 定位信息 用于
+	// TODO 服务端定位信息传递未完成
+	Weight uint32
 }
 type Event struct {
 }
